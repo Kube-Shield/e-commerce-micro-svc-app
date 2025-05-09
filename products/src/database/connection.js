a68e462e04085f29db99fa5abe659593687554b1
Ahmed Idani <ahmed.idani@insat.ucar.tn>
2025-05-09 05:24:28 +0100

    most stable version

--- products/src/database/connection.js
+++ products/src/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { DB_URL } = require("../config");
-
+const { ProductModel } = require("./models");
 module.exports = async () => {
   try {
     await mongoose.connect(DB_URL, {
@@ -8,7 +8,14 @@ module.exports = async () => {
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log("Db Connected");
+
+    console.log("Db Connected");    
+    const count = await ProductModel.countDocuments();
+    if (count === 0) {
+      await ProductModel.insertMany(sampleData);
+    }
+
+    // Load sample data after successful connection
   } catch (error) {
     console.log("Error ============");
     console.log(error);
